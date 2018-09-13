bde3cb98b8f9a628283262bc780e51db23d2acf0
Taylor <tay333@gmail.com>
2018-09-13 11:39:23 -0700

    editors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 #![recursion_limit = "256"] // needed for html! macro expansion
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 #[macro_use]
 extern crate yew;
 extern crate num_complex;
@@ -12,67 +15,60 @@ extern crate serde_derive;
 #[macro_use]
 extern crate ndarray;
 
-
 mod complex;
 mod qvm;
 
-use std::time::Duration;
-
-use yew::html::{App, Html, KeyData};
-use yew::{
-    initialize, run_loop,
-    services::{interval::IntervalService, Task},
-};
+use yew::html::{App, Html};
+use yew::{initialize, run_loop};
 
 use stdweb::traits::*;
 use stdweb::unstable::TryInto;
 use stdweb::web::document;
 use stdweb::web::html_element::{InputElement, TextAreaElement};
 
+enum State {
+    Ready,
+    Editing,
+    Error,
+}
 pub struct Model {
     qvm: qvm::QVM,
-    job: Option<Box<Task>>,
-    running: bool,
+    gate_state: State,
+    gate_edit: String,
+    program_state: State,
+    program_edit: String,
 }
 
 pub enum Msg {
     Noop,
     Reset,
-    Program,
-    Gates,
-    Start,
-    Stop,
     Next,
     Prev,
+
+    EditProgram,
+    SaveProgram,
+
+    EditGates,
+    SaveGates,
 }
 
-pub struct Context {
-    pub interval: IntervalService<Msg>,
-}
+struct Context {}
 
 fn main() {
     initialize();
     let mut app = App::new();
-    let context = Context {
-        interval: IntervalService::new(app.sender()),
-    };
     let model = Model {
-        job: None,
         qvm: qvm::QVM::new(),
-        running: false,
+        gate_state: State::Ready,
+        gate_edit: "".to_string(),
+        program_state: State::Ready,
+        program_edit: "".to_string(),
+
     };
-    app.mount(context, model, update, view);
+    app.mount(Context {}, model, update, view);
     run_loop();
 }
 
-fn get_input(id: &str) -> String {
-    let input: InputElement = document()
-        .get_element_by_id(id)
-        .unwrap()
-        .try_into()
-        .unwrap();
-    input.raw_value()
-}
 fn get_text(id: &str) -> String {
     let input: TextAreaElement = document()
         .get_element_by_id(id)
@@ -81,19 +77,35 @@ fn get_text(id: &str) -> String {
         .unwrap();
     input.value()
 }
-fn update(context: &mut Context, model: &mut Model, msg: Msg) {
+fn update(_: &mut Context, model: &mut Model, msg: Msg) {
     match msg {
         Msg::Noop => {}
         Msg::Reset => {
             model.qvm.reset();
         }
-        Msg::Gates => {
-            let gates = get_text("gates");
-            model.qvm.set_gates(&gates);
+        Msg::EditGates => {
+            model.gate_state = State::Editing;
         }
-        Msg::Program => {
-            let prog = get_input("program");
-            model.qvm.update(prog);
+        Msg::SaveGates => {
+            let gates = get_text("gates");
+            model.gate_state = if model.qvm.set_gates(&gates) {
+                State::Ready
+            } else {
+                model.gate_edit = gates;
+                State::Error
+            };
+        }
+        Msg::EditProgram => {
+            model.program_state = State::Editing;
+        }
+        Msg::SaveProgram => {
+            let prog = get_text("program");
+            model.program_state = if model.qvm.update(&prog) {
+                State::Ready
+            } else {
+                model.program_edit = prog;
+                State::Error
+            };
         }
         Msg::Prev => {
             model.qvm.prev();
@@ -101,65 +113,60 @@ fn update(context: &mut Context, model: &mut Model, msg: Msg) {
         Msg::Next => {
             model.qvm.next();
         }
-        Msg::Start => {
-            let timeout = Duration::from_millis(500);
-            let handle = context.interval.spawn(timeout, || Msg::Next);
-            model.job = Some(Box::new(handle));
-            model.running = true;
-        }
-        Msg::Stop => {
-            if let Some(mut task) = model.job.take() {
-                task.cancel();
-            }
-            model.job = None;
-            model.running = false;
-        }
     }
 }
 fn view(model: &Model) -> Html<Msg> {
-    let pos = " ".repeat(model.qvm.counter) + "^";
-    let program: String = model.qvm.program.iter().collect();
-    let gates = model.qvm.show_gates();
-
-    let controller = if model.running {
-        html! {
-        <button class="button", onclick=move|_| Msg::Stop,>{ "Stop" }</button> }
-    } else {
-        let keypress = |data: KeyData| {
-            //warn!("{}",data.key);
-            if data.key == "ArrowUp" {
-                Msg::Next
-            } else if data.key == "ArrowDown" {
-                Msg::Prev
-            } else {
-                Msg::Program
-            }
-        };
-        html! {
-          <div>
-            <div>{"gates"}</div>
-            <textarea id="gates", cols=30, rows=25, onkeypress=move|_| Msg::Gates,>{gates} </textarea>
-            <br></br>
-            <input id="program", type="text", onkeypress=keypress, value={&program},/><span>{"program"}</span>
-            <br></br>
-            <input type="text", disabled=true, value={&pos},/><span>{"position"}</span>
-            <br></br>
-            <button class="button", onclick=move|_| Msg::Reset,>{ "Reset" }</button>
-            <button class="button", onclick=move|_| Msg::Prev,>{ "Prev" }</button>
-            <button class="button", onclick=move|_| Msg::Next,>{ "Next" }</button>
-          </div>
-        }
+    let gates = match model.gate_state {
+        State::Ready => html! {
+            <button class="button", onclick=move|_| Msg::EditGates,>{"Edit Gates"}</button>
+        },
+        State::Editing => html! {
+            <div>
+                <button class="button", onclick=move|_| Msg::SaveGates,>{"Save Gates"}</button>
+                <textarea id="gates", cols=30, rows=25, onkeypress=move|_| Msg::EditGates,>{model.qvm.show_gates()} </textarea>
+            </div>
+        },
+        State::Error => html! {
+            <div>
+                <div>{"ERROR!"}</div>
+                <button class="button", onclick=move|_| Msg::SaveGates,>{"Save Gates"}</button>
+                <textarea id="gates", cols=30, rows=25, onkeypress=move|_| Msg::EditGates,>{&model.gate_edit}</textarea>
+            </div>
+        },
     };
+    let program = match model.program_state {
+        State::Ready => html! {
+            <button class="button", onclick=move|_| Msg::EditProgram,>{"Edit Program"}</button>
+        },
+        State::Editing => html! {
+            <div>
+                <button class="button", onclick=move|_| Msg::SaveProgram,>{"Save Program"}</button>
+                <textarea id="program", cols=30, rows=25, onkeypress=move|_| Msg::EditProgram,>{model.qvm.read_program()} </textarea>
+            </div>
+        },
+        State::Error => html! {
+            <div>
+                <div>{"ERROR!"}</div>
+                <button class="button", onclick=move|_| Msg::SaveProgram,>{"Save Program"}</button>
+                <textarea id="progam", cols=30, rows=25, onkeypress=move|_| Msg::EditProgram,>{&model.program_edit}</textarea>
+            </div>
+        },
+    };
+
 
     html! {
         <div>
             <section class="section",>
-              { controller }
-              <span class=("tag","is-primary"),> {"counter: "} { model.qvm.counter } </span>
-              <br></br>
-              <span class=("tag","is-primary"),> { &model.qvm } </span>
-              <br></br>
-
+                { gates }
+                <br></br>
+                { program }
+                <button class="button", onclick=move|_| Msg::Reset,>{ "Reset" }</button>
+                <button class="button", onclick=move|_| Msg::Prev,>{ "Prev" }</button>
+                <button class="button", onclick=move|_| Msg::Next,>{ "Next" }</button>
+                <span class=("tag","is-primary"),> {"counter: "} { model.qvm.counter } </span>
+                <br></br>
+                <span class=("tag","is-primary"),> { &model.qvm } </span>
+                <br></br>
             </section>
         </div>
     }
--- src/qvm.rs
+++ src/qvm.rs
@@ -1,23 +1,28 @@
-#![allow(dead_code)]
-
 use num_complex;
 use serde_json;
 use std::collections::BTreeMap;
 use std::fmt;
-
+use std::iter::FromIterator;
 use ndarray::prelude::*;
 
 type Complex = num_complex::Complex64;
 type Qubit = Array1<Complex>;
+type Qubyte = (Qubit, Qubit);
 
 type G1 = Array2<Complex>;
 
+#[derive(Serialize, PartialEq)]
+enum Instruction {
+    Malformed,
+    Single(String, String),
+    Double(String, String, String),
+}
+
 pub struct QVM {
     pub counter: usize,
-    //pub qb1: Qubit,
-    pub qb0: Qubit,
-    pub program: Vec<char>,
-    gates: BTreeMap<char, G1>,
+    pub qb: Qubyte,
+    program: Vec<Instruction>,
+    gates: BTreeMap<String, G1>,
 }
 
 const C0: Complex = Complex { re: 0.0, im: 0.0 };
@@ -34,7 +39,7 @@ fn y() -> G1 {
     array![[C0, -I], [I, C0]]
 }
 fn h() -> G1 {
-    let h = 1.0 / Complex{re: 2.0, im: 0.0}.sqrt();
+    let h = 1.0 / Complex { re: 2.0, im: 0.0 }.sqrt();
     array![[h, h], [h, -h]]
 }
 
@@ -52,33 +57,65 @@ fn apply(gate: &G1, qb: &Qubit) -> Qubit {
 impl QVM {
     pub fn new() -> QVM {
         let mut map = BTreeMap::new();
-        map.insert('x', x());
-        map.insert('y', y());
-        map.insert('z', z());
-        map.insert('h', h());
+        map.insert("x".into(), x());
+        map.insert("y".into(), y());
+        map.insert("z".into(), z());
+        map.insert("h".into(), h());
         QVM {
             counter: 0,
-            qb0: zero(),
-            program: "".chars().collect(),
+            qb: (zero(), zero()),
+            program: vec![Instruction::Single("x".into(), "0".into())],
             gates: map,
         }
     }
+    pub fn read_program(&self) -> String {
+        String::from_iter(self.program.iter().map(|inst| {
+            match inst {
+                Instruction::Single(gate, qb) => gate.clone() + " " + qb + "\n",
+                Instruction::Double(gate, a1, a2) => gate.clone() + " " + a1 + " " + a2 + "\n",
+                _ => "".into(),
+            }
+        }))
+    }
     pub fn reset(&mut self) {
         self.counter = 0;
-        self.qb0 = zero();
+        self.qb = (zero(), zero());
     }
-    pub fn update(&mut self, program: String) {
-        self.program = program.chars().collect();
+    pub fn update(&mut self, program: &str) -> bool {
+        let prog = program.lines().map(|line| {
+            let words = line.split_whitespace().collect::<Vec<&str>>();
+            match words.len() {
+                2 => Instruction::Single(words[0].into(), words[1].into()),
+                3 => Instruction::Double(words[0].into(), words[1].into(), words[2].into()),
+                _ => Instruction::Malformed,
+            }
+        }).collect::<Vec<Instruction>>();
+        if prog.contains(&Instruction::Malformed) {
+            return false
+        } else {
+            self.program = prog;
+            return true
+        };
     }
-    pub fn set_gates(&mut self, gates: &str) {
-        self.gates = serde_json::from_str(gates).unwrap(); }
+    pub fn set_gates(&mut self, gates: &str) -> bool {
+        match serde_json::from_str(gates) {
+            Ok(obj) => {
+                self.gates = obj;
+                true
+            },
+            Err(_) => {
+                false
+            }
+        }
+    }
     pub fn show_gates(&self) -> String {
         serde_json::to_string_pretty(&self.gates).unwrap()
     }
     fn operate(&mut self) {
-        let op = &self.program[self.counter];
-        let gate = &self.gates[op];
-        self.qb0 = apply(&gate, &self.qb0);
+        if let Instruction::Single(gate, _) = &self.program[self.counter] {
+            let gate = &self.gates[gate];
+            self.qb = (apply(&gate, &self.qb.0), self.qb.1.clone());
+        }
     }
     pub fn prev(&mut self) {
         if self.counter > 0 {
@@ -95,8 +132,7 @@ impl QVM {
 }
 
 impl fmt::Display for QVM {
-     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-         write!(f, "{}", self.qb0)
-         //write!(f, "({}, {})", self.qb0, self.qb1)
-     }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.qb.0)
+    }
 }
